c74482cefca1b8bdfc30452dbaa4abc6631da8a3
Kish Siva <rodes88@msn.com>
2019-04-16 20:57:34 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,9 +2,8 @@ def add_s(arr)
   counter = 0 
   array = []
   
-  while counter < (arr.count - 1) 
-    array = arr[counter] 
-    arr[counter] = array.concat("s")
+  arr.each do |letter|
+    letter = letter.concat("s")
       next if arr[counter] == arr[2]
     counter+=1
   end 
