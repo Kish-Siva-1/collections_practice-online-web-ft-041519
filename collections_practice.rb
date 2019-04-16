8575865f4835c4d174b7558178666a2de3cf1327
Kish Siva <rodes88@msn.com>
2019-04-16 20:19:56 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -22,26 +22,11 @@ def reverse_array(arr)
   arr.reverse
 end 
 
-  # Question 5
-  describe '#reverse_array' do
-    it 'reverse the order of an array of integers' do
-      expect(reverse_array([12, 4, 35])).to eq([35, 4, 12])
-    end
-  end
-
 def kesha_maker(arr)
   arr.each do |letter|
     letter[3]="$" 
   end
 end 
-
-  # Question 6
-  describe '#kesha_maker' do
-    # DO NOT USE .collect or .map. use .each to build a new array, like in the "my_each" lab!
-    it 'taking an array as an input, change the 3rd character of each element to a dollar sign.' do
-      expect(kesha_maker(["blake", "ashley", "scott"])).to eq(["bl$ke", "as$ley", "sc$tt"])
-    end
-  end
 
 def find_a(arr)
   arr.select do |letter|
@@ -66,10 +51,3 @@ def add_s(arr)
    
 end 
 
-  # Question 9
-  describe '#add_s' do
-    it 'Add an "s" to each word in the array except for the 2nd element in the array' do
-      expect(add_s(["hand","feet", "knee", "table"])).to eq(["hands","feet", "knees", "tables"])
-    end
-  end
-end
