6a43ddf91f69805c304b541a1e91d01ed4dc2dfd
Kish Siva <rodes88@msn.com>
2019-04-16 20:52:23 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -48,7 +48,7 @@ def add_s(arr)
   while counter < (arr.count - 1) 
     array = arr[counter] 
     arr[counter] = array.concat("s")
-      unless arr[counter] == arr[2]
+      next if arr[counter] == arr[2]
     counter+=1
   end 
    
